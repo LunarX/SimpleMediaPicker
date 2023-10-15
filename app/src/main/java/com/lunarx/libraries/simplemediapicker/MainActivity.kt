5c58bbc7edd708415caaca75c251b1b3bd2842ff
LunarX <32095402+LunarX@users.noreply.github.com>
2023-10-15 16:09:54 +0200

    add module library as a dependency to the test modudle

--- app/src/main/java/com/lunarx/libraries/simplemediapicker/MainActivity.kt
+++ app/src/main/java/com/lunarx/libraries/simplemediapicker/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.lunarx.libraries.simplemediapicker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lunarx.libraries.simplemediapicker.ui.theme.SimpleMediaPickerTheme
 
 class MainActivity : ComponentActivity() {
+
+    val mediaPicker = MediaPicker(this) {
+        Log.e("gibran", "addImage: it = ${it}")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,6 +28,14 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
+
+                    TextButton(onClick = {
+                        mediaPicker.chooseMedia(MediaPicker.FileType.IMAGE)
+                    },
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = "Salut")
+                    }
                 }
             }
         }
